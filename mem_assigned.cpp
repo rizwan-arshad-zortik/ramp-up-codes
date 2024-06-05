1fa4b8e023ceba40b4a66af13321d3575b7ca53f
rizwan-arshad-zortik <rizwan.arshad@zortik.com>
2024-06-05 11:06:47 +0500

    Create mem_assigned.cpp

---
+++ mem_assigned.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void assignMem(char **arr);
+int main()
+{
+    char *arr = nullptr;
+    assignMem(&arr);
+    std::cout<<"\nMem var after function call"<<arr<<std::endl;
+    char ch = 'A';
+    for (int i=0; i<10; i++)
+    {
+        arr[i] = ch + i;
+    }
+
+    for (int i=0; i<10; i++)
+    {
+        std::cout<<arr[i];
+    }
+
+    return 0;
+}
+
+void assignMem(char **arr)
+{
+    *arr = (char *) malloc(sizeof(char) * 10);
+    std::cout<<"Mem Assigned "<< arr <<std::endl;
+}
