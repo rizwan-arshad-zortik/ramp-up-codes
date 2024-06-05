3c028c4c3f2a7a0e6cf4aa48953e45f35d82f46d
rizwan-arshad-zortik <rizwan.arshad@zortik.com>
2024-06-05 11:13:45 +0500

    Create perfect_forwarding.cpp

---
+++ perfect_forwarding.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <utility> // for std::move
+
+ 
+
+void process(int& x) {
+
+    std::cout << "L-value reference\n";
+
+}
+
+ 
+
+void process(int&& x) {
+
+    std::cout << "R-value reference\n";
+
+}
+
+ 
+
+template<typename T>
+
+void forwarder(T&& arg) {
+
+    process(std::forward<T>(arg)); // Perfect forwarding
+
+}
+
+ 
+
+int main() {
+
+    int a = 5;
+
+    forwarder(a);             // L-value reference
+
+    forwarder(5);             // R-value reference
+
+    forwarder(std::move(a));  // R-value reference
+
+    return 0;
+
+}
+
+ 
